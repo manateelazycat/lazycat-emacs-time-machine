c6acff248f522dcda05e6a3b96297ec14689a4ac
Andy Stewart <lazycat.manatee@gmail.com>
2018-07-06 21:34:55 +0800

    Add auto-complete packages.

---
+++ site-lisp/config/init-auto-complete.el
@@ -0,0 +1,109 @@
+;;; init-auto-complete.el --- Init for auto complete
+
+;; Filename: init-auto-complete.el
+;; Description: Init for auto complete
+;; Author: Andy Stewart <andy@freedom>
+;; Maintainer: Andy Stewart <andy@freedom>
+;; Copyright (C) 2013, Andy Stewart, all rights reserved.
+;; Created: 2013-12-30 01:31:46
+;; Version: 0.1
+;; Last-Updated: 2013-12-30 01:31:46
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/init-auto-complete.el
+;; Keywords:
+;; Compatibility: GNU Emacs 24.3.50.1
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Init for auto complete
+;;
+
+;;; Installation:
+;;
+;; Put init-auto-complete.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'init-auto-complete)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET init-auto-complete RET
+;;
+
+;;; Change log:
+;;
+;; 2013/12/30
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+
+(require 'auto-complete-config)
+;; (if (executable-find "g++")
+;;     (require 'auto-complete-clang-extension))
+
+;;; Code:
+
+(add-to-list 'ac-dictionary-directories (concat lazycat-emacs-extension-dir "/auto-complete/dict"))
+(ac-config-default)
+(setq ac-use-quick-help nil)
+
+(setq-default
+ ac-sources
+ '(
+   ac-source-yasnippet
+   ac-source-imenu
+   ac-source-abbrev
+   ac-source-words-in-same-mode-buffers
+   ac-source-files-in-current-dir
+   ac-source-filename
+   ;; ac-source-c-headers
+   ;; ac-source-rtags
+   ))
+
+(provide 'init-auto-complete)
+
+;;; init-auto-complete.el ends here
